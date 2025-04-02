a1a53b7bccaca941e27017ae61cd6b157f4dc6e2
Jason Ashton <jason.ashton@saberin.com>
2025-04-02 14:19:15 -0400

    add contants example

---
+++ CBP.Data/Enums/BusinessTypeConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace CBP.Data
+{
+    public static class BusinessTypeConstants
+    {
+        public const string Corporation = "Corporation";
+        public const string LimitedLiabilityCompany = "LimitedLiabilityCompany";
+        public const string NonProfit = "NonProfit";
+        public const string Cooperative = "Cooperative";
+        public const string PublicBenefitCorporation = "PublicBenefitCorporation";
+        public const string JointVenture = "JointVenture";
+        public const string ServiceBusiness = "ServiceBusiness";
+        public const string Partnership = "Partnership";
+        public const string SoleProprietorship = "SoleProprietorship";
+        public const string LimitedLiabilityPartnership = "LimitedLiabilityPartnership";
+    }
+}
